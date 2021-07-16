74f45d7894db8be81db9fba8a728176a3582c8dc
Lawrence Chen <lochen@mednet.ucla.edu>
2021-07-15 17:50:07 -0700

    11_archr_export_peaks: add cluster 2 peaks

--- src/11_archr_export_peaks.R
+++ src/11_archr_export_peaks.R
@@ -6,7 +6,7 @@ l <- lapply(liblist, function(x) suppressPackageStartupMessages(require(x, chara
 base_dir <- normalizePath("../data/archr/atac-2020-all")
 out_dir <- file.path(base_dir, "11_archr_peaks_export")
 
-project_names <- c("precg_C7", "insula_C7")
+project_names <- c("precg_C7", "insula_C7", "precg_C2", "insula_C2")
 archr_project <- setNames(file.path(base_dir, paste0("peak_calling_", project_names)), project_names)
 
 RESOURCES <- list(
@@ -28,4 +28,8 @@ export_peaks <- function(archr_project, project_name) {
     dir.create(file.path(out_dir), showWarnings = FALSE, recursive = TRUE)
     project <- loadArchRProject(archr_project)
     rtracklayer::export(getPeakSet(project), file.path(out_dir, str_glue("{project_name}.bed")), "BED")
+}
+
+if (!interactive()) {
+    main()
 }
