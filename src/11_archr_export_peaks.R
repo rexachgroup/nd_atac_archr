e6430195fb5adc066dc532a82dc2d27f2e1631c3
Lawrence Chen <lochen@mednet.ucla.edu>
2021-08-02 09:40:51 -0700

    11_archr_export_peaks: delete batchtools

--- src/11_archr_export_peaks.R
+++ src/11_archr_export_peaks.R
@@ -9,14 +9,6 @@ out_dir <- file.path(base_dir, "11_archr_peaks_export")
 project_names <- c("precg_C7", "insula_C7", "precg_C2", "insula_C2")
 archr_project <- setNames(file.path(base_dir, paste0("peak_calling_", project_names)), project_names)
 
-RESOURCES <- list(
-    ncpus = 1,
-    memory = 4,
-    walltime = 86400,
-    partition = "bigmem"
-)
-
-
 main <- function() {
     dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
     addArchRThreads(16)
