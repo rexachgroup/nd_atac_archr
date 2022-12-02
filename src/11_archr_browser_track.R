c238e5da3c13d1986f167580e25b4fed89ee9ae2
Lawrence Chen <lochen@mednet.ucla.edu>
2022-12-02 15:20:43 -0800

    11_archr_browser_track: do only insula-c25, group by subclusters

--- src/11_archr_browser_track.R
+++ src/11_archr_browser_track.R
@@ -1,19 +1,12 @@
 # browser tracks centered around certain motifs / peaks.
-liblist <- c("tidyverse", "batchtools", "readxl", "Seurat", "ArchR", "GenomicRanges", "patchwork")
+liblist <- c("tidyverse", "readxl", "Seurat", "ArchR", "GenomicRanges", "patchwork")
 l <- lapply(liblist, require, character.only = TRUE, quietly = TRUE)
 
 base_dir <- normalizePath("../data/archr/atac-2020-all")
 out_dir <- file.path(base_dir, "11_archr_browser_track/")
 batchtools <- file.path(out_dir, "batchtools")
 archr_project <- list(
-    "precg_C2" = file.path(base_dir, "10_tf_transfac_c2", "chrom_var_precg_C2"),
-    "insula_C2" = file.path(base_dir, "10_tf_transfac_c2", "chrom_var_insula_C2"),
-    "subcluster_peak_call_insula-c25" = file.path(base_dir, "09_archr_subcluster_peak_calling", "peak_call_insula-c25"),
-    "subcluster_peak_call_precg-c25" = file.path(base_dir, "09_archr_subcluster_peak_calling", "peak_call_precg-c25"),
-    "subcluster_peak_call_insula-c6789" = file.path(base_dir, "09_archr_subcluster_peak_calling", "peak_call_insula-c6789"),
-    "subcluster_peak_call_precg-c6789" = file.path(base_dir, "09_archr_subcluster_peak_calling", "peak_call_precg-c6789")
-
-    
+    "subcluster_peak_call_insula-c25" = file.path(base_dir, "09_archr_subcluster_peak_calling", "peak_call_insula-c25")
 )
 
 peak_tb <- tribble(
@@ -22,12 +15,17 @@ peak_tb <- tribble(
     "PSAP",     "chr10",    71816298,   71851375,   67,
     "PSAP",     "chr10",    71816298,   71851375,   -9263,
     "C9orf72",  "chr9",     27546545,   27573866,   -346,
-    "VCP",      "chr9",     35056064,   35056064,  1150
+    "VCP",      "chr9",     35056064,   35073249,  1150
 )
 
-plot_param <- tibble(
-    tileSize = c(50)
+peak_tb <- tribble(
+    ~name,      ~seqnames,  ~start,     ~end,       ~offset,
+    "VCP",      "chr9",     35056064,   35073249,  1150
 )
+
+# plot_param <- tibble(
+#     tileSize = c(10, 50, 100)
+# )
 
 main <- function() {
     
@@ -35,11 +33,6 @@ main <- function() {
     setwd(out_dir)
     
     dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
-    if (!dir.exists(batchtools)) {
-        reg <- makeRegistry(batchtools)
-    } else {
-        reg <- loadRegistry(batchtools, writeable = TRUE)
-    }
 
     tb <- tibble(
         project_names = names(archr_project),
@@ -68,19 +61,41 @@ main <- function() {
         bind_rows()
     plot_gr <- GRanges(peak_coords)
 
+    #     tb <- tb %>%
+    #         mutate(track_obj = pmap(., function(...) {
+    #             cr <- list(...)
+    #             pmap(plot_param, function(...) {
+    #                 pr <- list(...)
+    #                 map(1:length(plot_gr), function(range_i) {
+    #                     gr <- plot_gr[range_i,]
+    #                     title <- str_glue("{cr$project_names} {deparse(pr)} {gr$name} atac signal sum, split dx")
+    #                     track_obj <- plotBrowserTrack(cr$proj, region = gr, groupBy = "Clusters", ...)
+    #                     print(title)
+    #                     return(wrap_plots(track_obj) + plot_annotation(title = title))
+    #                 })
+    #             })
+    #         }))
+
     tb <- tb %>%
         mutate(track_obj = pmap(., function(...) {
             cr <- list(...)
-            pmap(plot_param, function(...) {
-                pr <- list(...)
+            group_meta <- cr$proj@cellColData %>%
+                as.data.frame %>%
+                rownames_to_column("barcode") %>%
+                group_by(Clusters) %>%
+                group_nest(keep = T)
+            pmap(group_meta, function(...) {
+                m <- list(...)
+                subset_proj <- cr$proj[, m$barcode]
                 map(1:length(plot_gr), function(range_i) {
                     gr <- plot_gr[range_i,]
-                    title <- str_glue("{cr$project_names} {deparse(pr)} {gr$name} atac signal sum, split dx")
-                    track_obj <- plotBrowserTrack(cr$proj, region = gr, groupBy = "Clinical.Dx", ...)
+                    title <- str_glue("{cr$project_names} {gr$name} {m$Clusters} atac signal sum, split dx") 
+                    track_obj <- plotBrowserTrack(cr$proj, region = gr, groupBy = "Clinical.Dx", tileSize = 10)
                     print(title)
                     return(wrap_plots(track_obj) + plot_annotation(title = title))
-                })
+                }) 
             })
+
         }))
 
     pwalk(tb, function(...) {
